7f1e87524125f51474a75a5b729c92775e6c154c
Joyoshish Saha <34447725+Joyoshish@users.noreply.github.com>
2021-04-10 11:57:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-
 import streamlit as st
 import json
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
+#checked
 
 URI = 'http://127.0.0.1:5000'
 st.title('Neural Network Visualizer')
